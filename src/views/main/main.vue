ec16dbefe909bf9d0b690c1a76582d2a15f3fb31
Anani 2017 <miyiyangdezixin@163.com>
2018-07-25 11:35:41 +0800

    initial commit webpack-iview-vue

---
+++ src/views/main/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{test}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test: this.$store.state.count
+        }
+    }
+}
+</script>
+
+<style lang=less>
+
+</style>
