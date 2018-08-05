668231a3a0e00275ebd66ddf273247b19f90fccb
Anani 2017 <miyiyangdezixin@163.com>
2018-08-06 00:39:19 +0800

    update

---
+++ src/components/bowen/bowen.vue
@@ -0,0 +1,31 @@
+<template>
+    <Row class="bo-wen">
+        <Col span="4"><slot name="catalog"></slot>&nbsp;</Col>
+        <Col class="h-100 outContainer" span="17">
+            <div class="h-100 innerContainer">
+               <slot></slot>
+            </div>
+        </Col>
+        <Col span="3"><slot name="others">&nbsp;</slot></Col>
+    </Row>
+</template>
+
+<script>
+export default {
+    name: 'BoWen'
+}
+</script>
+
+<style lang="less" scoped>
+    .bo-wen {
+        height: calc(100% - 90px);
+        .outContainer {
+            overflow: hidden;
+        }
+        .innerContainer {
+            overflow-x: hidden;
+            overflow-y: scroll;
+            width: calc(100% + 17px);
+        }
+    }
+</style>
--- src/views/articles/others/notes/note-00.vue
+++ src/views/articles/others/notes/note-00.vue
@@ -1,69 +1,56 @@
 <template>
-    <Row class="bo-wen">
-        <Col span="4">目录</Col>
-        <Col class="pt-3 h-100 outContainer" span="17">
-            <div class="h-100 innerContainer">
-                <h3>注册全局可用的函数</h3>
-                <pre v-highlight>
-                    <code class="js">// main.js
+    <BoWen>
+        <div style="background: rgb(35, 36, 31)">
+            <div class="py-3 text-center">
+                <Button type="primary"><h1>笔记(一) - 全局注册组件和函数</h1></Button>
+            </div>
+            <Tag class="ml-1 font-weight-bold" type="dot" color="primary">注册全局可用的函数</Tag>
+            <Divider />
+            <pre v-highlight>
+                <code class="js">// main.js
     import util from './util.js'
     Vue.prototype.$util = util;</code>
-                </pre>
-                <pre v-highlight>
-                    <code class="js">// 在组件中使用
+            </pre>
+            <pre v-highlight>
+                <code class="js">// 在组件中使用
     method:{
-        helloWorld() {
-            // sayHello 为 util 上的方法
-            this.$util.sayHello();
-        }
+    helloWorld() {
+        // sayHello 为 util 上的方法
+        this.$util.sayHello();
+    }
     }</code>
-                </pre>
-                <h3>注册全局组件</h3>
-                <pre v-highlight>
-                    <code class="js">// main.js
+            </pre>
+            <Tag class="ml-1 font-weight-bold" type="dot" color="primary">注册全局组件</Tag>
+            <Divider />
+            <pre v-highlight>
+                <code class="js">// main.js
     // 自定义全局组件
     import defineSubassemblies from './components/defineSubassembly.js';
     Vue.use(defineSubassemblies);</code>
-                </pre>
-                <pre v-highlight>
-                    <code class="js">// defineSubassembly.js
+            </pre>
+            <pre v-highlight>
+                <code class="js">// defineSubassembly.js
     import FullPage from './fullpage/fullpage.vue';
     import PageItem from './fullpage/components/page.vue';
 
     const defineSubassemblies = {
-        install:function (Vue) {
-            Vue.component('FullPage',FullPage);
-            Vue.component('PageItem',PageItem);
-        }
+    install:function (Vue) {
+        Vue.component('FullPage',FullPage);
+        Vue.component('PageItem',PageItem);
+    }
     };
 
     export default defineSubassemblies;
     </code>
-                </pre>
-                <div class="text-center text-white">到底了，我可是有底线的。<Icon type="ios-happy-outline" /></div>
-            </div>
-        </Col>
-        <Col span="3">额外的内容</Col>
-    </Row>
+            </pre>
+            <FooterDivider></FooterDivider>
+        </div>
+    </BoWen>
 </template>
 
 <script>
 export default {
-    name: 'BoWen'
+    name: 'NoteOne'
 }
 </script>
-
-<style lang="less" scoped>
-    .bo-wen {
-        height: calc(100% - 90px);
-        .outContainer {
-            overflow: hidden;
-        }
-        .innerContainer {
-            overflow-x: hidden;
-            overflow-y: scroll;
-            width: calc(100% + 17px);
-        }
-    }
-</style>
 
