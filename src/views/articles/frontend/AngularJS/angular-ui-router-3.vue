05ba1974f2ae97d46e6ab47d9f6622379a6c1223
Anani1994 <dongwhchn@163.com>
2018-10-17 23:04:23 +0800

    update angular-ui-router-3.vue & angular-nootes.vue

--- src/views/articles/frontend/AngularJS/angular-ui-router-3.vue
+++ src/views/articles/frontend/AngularJS/angular-ui-router-3.vue
@@ -6,6 +6,23 @@
                     content="嵌套路由"
                     status="wait"
                     @click.native="toTop('#angular-ui-router-3-1', 30)")
+                Step(title="多命名视图"
+                    content="多命名视图"
+                    status="wait"
+                    @click.native="toTop('#angular-ui-router-3-2', 30)")
+                    Steps(direction="vertical")
+                        Step(title="基本使用"
+                            content="基本使用"
+                            status="wait"
+                            @click.native.stop="toTop('#angular-ui-router-3-2-1', 30)")
+                        Step(title="相对命名和绝对命名"
+                            content="相对命名和绝对命名"
+                            status="wait"
+                            @click.native.stop="toTop('#angular-ui-router-3-2-2', 30)")
+                        Step(title="继承依赖"
+                            content="继承依赖"
+                            status="wait"
+                            @click.native.stop="toTop('#angular-ui-router-3-2-3', 30)")
         .px-1
             .py-3.text-center
                 h1
@@ -94,8 +111,146 @@
             p.text-info 当子状态被激活时，将会加载其 template 显示在父级的 ui-view 中，当没被指定父级状态时，显示在index.html 的 ui-view 中。
             h3#angular-ui-router-3-2.ml-1
                 Icon(type="logo-buffer")
-                | &nbsp;嵌套路由
+                | &nbsp;多命名视图
             Divider
+            p 你可以给的视图进行命名，这样在一个模板里你可以拥有多个视图。
+            h4#angular-ui-router-3-2-1.ml-1
+                Icon(type="logo-buffer")
+                | &nbsp;基本使用
+            Divider
+            p 要创建命名视图只需要在设置路由对象时添加 views 属性。需要注意的是如果你设置了 views 属性，那么路由的 template，templateUrl，templateProvider 将会被忽略，因此你可以创建一个抽象路由来包含模板，子路由都定义在 views 属性里。
+            pre.ml-2(v-highlight)
+                code.html &lt;!-- index.html --&gt;
+                    |
+                    | &lt;body&gt;
+                    | &lt;div ui-view="viewName1"&gt;&lt;/div&gt;	
+                    | &lt;div ui-view="viewName2"&gt;&lt;/div&gt;
+                    | &lt;div ui-view="viewName3"&gt;&lt;/div&gt;
+                    | &lt;/body&gt;
+            pre.ml-2(v-highlight)
+                code.js $stateProvider
+                    | 
+                    |     .state('report', {
+                    |         views: {
+                    |             'viewName1': {},
+                    |             'viewName2': {},
+                    |             'viewName3': {}
+                    |         }
+                    |     });
+            p 在每个视图中可以设置自己的模板(template，templateUrl，templateProvider)和控制器(controller)。
+            pre.ml-2(v-highlight)
+                code.js $stateProvider
+                    | 
+                    |     .state('report', {
+                    |         views: {
+                    |             'viewName1': {
+                    |                 templateUrl: 'view1.html',
+                    |                 controller: 'ViewOneCtrl'
+                    |             },
+                    |             'viewName2': {
+                    |                 templateUrl: 'view2.html',
+                    |                 controller: 'ViewTwoCtrl'
+                    |             },
+                    |             'viewName3': {
+                    |                 templateUrl: 'view3.html',
+                    |                 controller: 'ViewThreeCtrl'
+                    |             }
+                    |         }
+                    |     });
+            h4#angular-ui-router-3-2-2.ml-1
+                Icon(type="logo-buffer")
+                | &nbsp;相对命名和绝对命名
+            Divider
+            p 在定义 views 属性时，如果视图名称中包含 @，那么视图名称就是绝对命名的方式，否则就是相对命名的方式。
+            p 相对命名的意思是相对于父模板中的 ui-view，而绝对命名则指定了相对于哪个状态的模板。
+            p 在定义 views 属性后，其内部如果包含了 @，那么 @ 前面部分为空表示未命名的 ui-view，@ 后面为空则表示相对于根模板，通常是 index.html。
+            p.text-warn 请注意，一旦使用了 @ 则表示绝对命名的方式。
+            pre.ml-2(v-highlight)
+                code.html &lt;!-- index.html --&gt;
+                    | 
+                    | &lt;body ng-app&gt;
+                    | &lt;div ui-view&gt;&lt;/div&gt;
+                    | &lt;div ui-view="status"&gt;&lt;/div&gt;
+                    | &lt;/body&gt;
+                    | &lt;!-- products.html --&gt;
+                    | &lt;h1&gt;My Products&lt;/h1&gt;
+                    | &lt;div ui-view&gt;&lt;/div&gt;
+                    | &lt;div ui-view="detail"&gt;&lt;/div&gt;
+                    | &lt;!-- products.detail.html --&gt;
+                    | &lt;h1&gt;Products Details&lt;/h1&gt;
+                    | &lt;div ui-view="info"&gt;&lt;/div&gt;
+            pre.ml-2(v-highlight)
+                code.js $stateProvider
+                    | 
+                    |     .state('products', {
+                    |         // 加载到 index.html 中未命名视图(ui-view)中
+                    |         templateUrl: 'products.html'
+                    |     })
+                    |     .state('products.detail', {
+                    |         views: {
+                    |             // 相对命名
+                    |             // products.html 中 &lt;div ui-view='detail'/&gt;
+                    |             "detail": {},
+                    |             // 相对命名
+                    |             // 对应 products.html 中的未命名 ui-view &lt;div ui-view/&gt;
+                    |             "": {},
+                    |             // 绝对命名
+                    |             // 对应 products.detail.html 中 &lt;div ui-view='info'/&gt;
+                    |             "info@products.detail": {},
+                    |             // 绝对命名
+                    |             // 对应 products.html 中 &lt;div ui-view='detail'/&gt;
+                    |             "detail@products": {},
+                    |             // 绝对命名
+                    |             // 对应 products.html 中的未命名视图： &lt;div ui-view/&gt;
+                    |             "@products": {},
+                    |             // 绝对命名
+                    |             // 对应根模板(index.html) 中名为 status 的视图： &lt;div ui-view='status'/&gt;
+                    |             "status@": {},
+                    |             // 绝对命名
+                    |             // 对应 index.html 中未命名视图：&lt;div ui-view/&gt;
+                    |             "@": {}
+                    |         }
+                    |     });
+            h4#angular-ui-router-3-2-3.ml-1
+                Icon(type="logo-buffer")
+                | &nbsp;继承依赖
+            Divider
+            p 视图可以从它们所属的状态继承解析的依赖关系，但可能不继承它们的兄弟视图的那些。
+            pre.ml-2(v-highlight)
+                code.js $stateProvider.state('multipleViews', {
+                    | 
+                    |     resolve: {
+                    |         data1: function () {
+                    |             return {};
+                    |         }
+                    |     },
+                    |     views: {
+                    |         'view1@multipleViews': {
+                    |             templateUrl: 'multiple-views-view1.html',
+                    |             controller: function ($scope, data1, data2) {
+                    |                 /* has access to data1 and data2,
+                    |                    but *not* data3 */
+                    |             },
+                    |             resolve: {
+                    |                 data2: function () {
+                    |                     return {};
+                    |                 }
+                    |             },
+                    |         },
+                    |         'view2@multipleViews': {
+                    |             templateUrl: 'multiple-views-view2.html',
+                    |             controller: function ($scope, data1, data3) {
+                    |                 /* has access to data1 and data3,
+                    |                    but *not* data2 */
+                    |             },
+                    |             resolve: {
+                    |                 data3: function () {
+                    |                     return { value: 'view2' };
+                    |                 },
+                    |             },
+                    |         },
+                    |     }
+                    | });
             p.text-right 还没有看够？请点击
                 a(href="/#/articles/frontend/AngularJS/angular-ui-router-4")  UI-Router for AngularJS (五) 
                 | 查看更多内容。
